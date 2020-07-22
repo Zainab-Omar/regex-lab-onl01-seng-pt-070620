3550a31323d53a520ac998642cc4e590dc18220b
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-22 18:53:39 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,10 +1,5 @@
 def starts_with_a_vowel?(word)
-  word.each do |word|
-    if word.scan (/^[^aeiou\W]i)
-      return true
-    else false
-  end
-end
+  word.match (/^[aioue]\w/i)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
