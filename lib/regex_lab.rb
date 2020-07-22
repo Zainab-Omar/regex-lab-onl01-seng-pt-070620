feb43aabe46063520222eb8923052e47914c6cf7
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-22 18:41:35 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,10 @@
 def starts_with_a_vowel?(word)
   word.each do |word|
-    if word.match ()
+    if word.scan (/^[^aeiou\W]i)
+      return true
+    else false
+  end
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
